8ff5ba49c34153f25e32a9ab0eabc5878813742a
KChaitanyaP <>
2024-05-06 21:46:59 +0530

    testing secrets in workflow

--- src/print_env.py
+++ src/print_env.py
@@ -6,6 +6,7 @@ cloud_function_name = os.environ.get("CLOUD_FUNCTION_NAME")
 cloud_function_directory = os.environ.get("CLOUD_FUNCTION_DIRECTORY")
 credentials = os.environ.get("CREDENTIALS")
 debug_mode = os.environ.get("DEBUG_MODE")
+token = os.environ.get("TOKEN")
 
 # to set output, print to shell in following syntax
 # print(f"::set-output name=num_squared::{num ** 2}")
@@ -15,3 +16,4 @@ print("cloud_function_name", cloud_function_name)
 print("cloud_function_directory", cloud_function_directory)
 print("credentials", credentials)
 print("debug_mode", debug_mode)
+print("token", token)
